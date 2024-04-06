fb3d40d561923081f111b3d85b46dbffa588051d
funky-eyes <jianbin@apache.org>
2024-04-06 16:10:59 +0800

    feature: support set nx ex protocol

--- src/main/java/org/redis2asp/protocol/RedisCommandDecoder.java
+++ src/main/java/org/redis2asp/protocol/RedisCommandDecoder.java
@@ -52,7 +52,7 @@ public class RedisCommandDecoder implements CommandDecoder {
 
     private RedisRequest<?> convert2RedisRequest(List<String> params) {
         String cmd = params.get(0);
-        LOGGER.info("cmd: {}", cmd);
+        LOGGER.info("cmd: {}", params);
         switch (cmd.toLowerCase()) {
             case "get":
                 return new GetRequest(params.get(1));
