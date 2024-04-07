66eeda0d42aed3cdd35b3b26766d01b02ab6b85f
jianbin <jianbin@apache.org>
2024-04-07 17:01:39 +0800

    feature: support hset protocol

--- src/test/java/icu/funkye/redispike/ServerTest.java
+++ src/test/java/icu/funkye/redispike/ServerTest.java
@@ -46,18 +46,26 @@ public class ServerTest {
     @BeforeAll
     public static void init() throws ParseException {
         server = new Server();
-        server.start("-th 10.58.10.103 -tp 3000 -n test -s tdkv-test -TU tongdun-admin1 -TP xxxzzz123 -p 6789"
+        server.start("-p 6789"
             .split(" "));
         aspClient = AeroSpikeClientFactory.getClient();
     }
 
     @Test
     public void testhHset() {
-        try (Jedis jedis2 = new Jedis("127.0.0.1", 6789)) {
-            Map<String, String> a = new HashMap<>();
-            Long result = jedis2.hset("a".getBytes(StandardCharsets.UTF_8), "b".getBytes(StandardCharsets.UTF_8),
+        try (Jedis jedis = new Jedis("127.0.0.1", 6789)) {
+            Long result = jedis.hset("a".getBytes(StandardCharsets.UTF_8), "b".getBytes(StandardCharsets.UTF_8),
                 "c".getBytes(StandardCharsets.UTF_8));
             Assertions.assertEquals(result, 1);
+            Map<String, String> map = new HashMap<>();
+            map.put("b", "c");
+            map.put("d", "e");
+            result = jedis.hset("a", map);
+            Assertions.assertEquals(result, 2);
+            result = jedis.hsetnx("a","f","g");
+            Assertions.assertEquals(result, 1);
+            result = jedis.hsetnx("a","f","g");
+            Assertions.assertEquals(result, 0);
         }
     }
 
